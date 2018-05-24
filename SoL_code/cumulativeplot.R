f775a0177b6139d9552f30876a30b0ebc816e66b
smc322 <smc322@cornell.edu>
2018-05-24 07:42:20 -0400

    code to make cumulative freq plot

---
+++ SoL_code/cumulativeplot.R
@@ -0,0 +1,7 @@
+#plot cumulative number of observations for each variable - similar to obs_per_lake.png
+
+# this looks like it will work
+# http://www.r-tutor.com/elementary-statistics/quantitative-data/cumulative-relative-frequency-distribution
+# http://www.r-tutor.com/elementary-statistics/quantitative-data/cumulative-frequency-graph
+
+data<-readRDS(file="SoL_data/SoL_data.rds")
