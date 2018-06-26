eb82d36d2f54190cfd4f24a3ce08449fb695397c
smc322 <smc322@cornell.edu>
2018-06-26 10:24:25 -0400

    failure at attempting to use density/sample to unbias distribution

---
+++ SoL_code/UnbiasDistribution.R
@@ -0,0 +1,40 @@
+##SMC June 2018
+## want to sample from lakes with chem data distribution to create same distribution as the census
+## i.e., un-bias the data to see if chemisty is different in our biased sample of lakes compared to all lakes
+
+# one approach - bin data and sample to have same # in the bin as we'd expect for distribution
+# seems cumbersome
+# alternatively, try something like this:
+# https://stackoverflow.com/questions/41495240/how-to-sample-data-based-off-the-distribution-of-another-dataset-in-r
+
+
+#read latest dataset and dataset that has lake area and additional variables
+
+c.data<-readRDS(file="SoL_data/SoL_data.rds")
+l.data<-readRDS(file="SoL_data/lakes.rds")
+
+l4.data<-l.data[l.data$lake_area_ha>=4,]
+
+l.c.data<-l.data[l.data$lagoslakeid %in% c.data$lagoslakeid, ]
+lc4.data<-l.c.data[l.c.data$lake_area_ha>=4,]
+
+hist(log(l4.data$lake_area_ha))
+hist(log(lc4.data$lake_area_ha))
+
+#theywant to get model dist from obs dist
+# Obs <- rnorm(1000, 5, 2.5)
+# Model <- rnorm(10000, 8, 1.5)
+# dens.obs <- density(Obs, n=length(Model))
+# predict_density <- approxfun(dens.obs)
+# SampleMod3 <- sample(Model, length(Obs), replace=FALSE, prob=predict_density(Model))
+
+
+Biased<-round(log(l.c.data$lake_area_ha, digits=0))
+Unbiased<-round(log(l.data$lake_area_ha, digits=0))
+dens.bias <- density(Biased)
+dens.unbias <- density(Unbiased)
+predict_density <-approxfun(dens.unbias)
+#probvec<-dens.obs$y
+newsample <- sample(Biased, 400, replace=FALSE, prob=predict_density(Unbiased))
+hist(log(SampleMod3))
+                     
