1a17ab044163a3c8ed15cfd50b1ea9f060c0c53f
Samantha Oliver <soliver@usgs.gov>
2018-06-27 14:20:24 -0500

    added dotplot of single proportion sampled statistic.

--- SoL_code/dotplots.R
+++ SoL_code/dotplots.R
@@ -149,46 +149,24 @@ legend(-.9,1.05, legend = c("Population", "TN Sample", "TP Sample", "Chl Sample"
        pch = 16, pt.cex = 2, bty = "n", col = c("black", my.cols), cex =1, horiz = TRUE)
 dev.off()
 
+# just proportion of lakes sampled
 png("SoL_graphics/proportion_sampled_state_dotplot.png", height = 600, width = 400)
 #par(mfrow=c(1,4), cex = 1, oma = c(0,5,3,3))
-#par(mar=c(5,0,0,0))
+par(mar=c(5,7,1,1), oma = c(0, 5, 4, 0))
 
 # dotplot of proportion of lakes sampled
-dotchart2(tn_summ$prop_count, labels = pop_summ$state_name,  dotsize = 2, xlab = "Proportion of lakes sampled", 
-          xlim = c(0, 0.3), bty= "L", width.factor = .2, col = my.cols[1])
-dotchart2(tp_summ$prop_count,  dotsize = 2, add = TRUE, col = my.cols[2])
-dotchart2(chl_summ$prop_count,  dotsize = 2, add = TRUE, col = my.cols[3])
-dotchart2(doc_summ$prop_count,  dotsize = 2, add = TRUE, col = my.cols[4])
+dotchart2(tn_summ$prop_sampled, labels = pop_summ$state_name,  dotsize = 2, xlab = "Proportion of lakes sampled", 
+          xlim = c(0, 0.7), bty= "L", width.factor = .2, col = my.cols[1])
+dotchart2(tp_summ$prop_sampled,  dotsize = 2, add = TRUE, col = my.cols[2])
+dotchart2(chl_summ$prop_sampled,  dotsize = 2, add = TRUE, col = my.cols[3])
+dotchart2(doc_summ$prop_sampled,  dotsize = 2, add = TRUE, col = my.cols[4])
 
-# lake area
-dotchart2(pop_summ$lake_area_ha, labels = "",dotsize = 2, xlab = "Area (ha)", xlim = c(0, 600), 
-          bty= "L", width.factor = .2, col = add.alpha('black', 0.7))
-dotchart2(tn_summ$lake_area_ha,  dotsize = 2, add = TRUE, col = my.cols[1])
-dotchart2(tp_summ$lake_area_ha,  dotsize = 2, add = TRUE, col = my.cols[2])
-dotchart2(chl_summ$lake_area_ha,  dotsize = 2, add = TRUE, col = my.cols[3])
-dotchart2(doc_summ$lake_area_ha,  dotsize = 2, add = TRUE, col = my.cols[4])
-
-# residence time proxy or WSA/LA
-dotchart2(pop_summ$res_time_proxy, labels = "", xlab = "WSA:LA", dotsize = 2, xlim = c(0, 150), 
-          bty= "L", width.factor = .2, col = add.alpha('black', 0.7))
-dotchart2(tn_summ$res_time_proxy,  dotsize = 2, add = TRUE, col = my.cols[1])
-dotchart2(tp_summ$res_time_proxy,  dotsize = 2, add = TRUE, col = my.cols[2])
-dotchart2(chl_summ$res_time_proxy,  dotsize = 2, add = TRUE, col = my.cols[3])
-dotchart2(doc_summ$res_time_proxy,  dotsize = 2, add = TRUE, col = my.cols[4])
-
-# proportion of sampled or population lakes in each state that are isolated
-dotchart2(pop_summ$prop_iso, labels = "", xlab = "Proportion isolated lakes", dotsize = 2, xlim = c(-0.02, 0.5), 
-          bty= "L", width.factor = .2, col = add.alpha('black', 0.7))
-dotchart2(tn_summ$prop_iso,  dotsize = 2, add = TRUE, col = my.cols[1])
-dotchart2(tp_summ$prop_iso,  dotsize = 2, add = TRUE, col = my.cols[2])
-dotchart2(chl_summ$prop_iso,  dotsize = 2, add = TRUE, col = my.cols[3])
-dotchart2(doc_summ$prop_iso,  dotsize = 2, add = TRUE, col = my.cols[4])
 
 # add legend
 par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
 plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-legend(-.9,1.05, legend = c("Population", "TN Sample", "TP Sample", "Chl Sample", "DOC Sample"), 
-       pch = 16, pt.cex = 2, bty = "n", col = c("black", my.cols), cex =1, horiz = TRUE)
+legend(-0.6,1, legend = c("TN Sample", "TP Sample", "Chl Sample", "DOC Sample"), 
+       pch = 16, pt.cex = 2, bty = "n", col = my.cols, cex =1,ncol = 2)
 dev.off()
 
 
