6ff98bab03db88fbd991bdeb3d3573406dec0ae4
Sarah Collins <smc322@cornell.edu>
2018-07-31 11:36:56 -0500

    pretty sure this still doesn't work?

--- SoL_code/resampling_timeseries.R
+++ SoL_code/resampling_timeseries.R
@@ -1,4 +1,4 @@
-##want to do a basic resampling exercise with lakes that have at least 20 years of data -- how many years of data are required for mean and variance to approach "true" values from actual 20 yr time series
+##want to do a basic resampling exercise with lakes that have at least 20 years of data -- how many years of data are required for mean and variance to approach "true" values from actual 20 yr time series.  actual values for mean and variance are specific by lagoslakeid, so calculate the absolute value of the % difference betweena "true" average and average based on small/resampled data.
 
 library(dplyr)
 
@@ -6,48 +6,80 @@ data = readRDS("SoL_data/SoL_data.rds")
 
 #try this with secchi first b/c lots of LT data
 
-sec.dat = na.omit(data[,c("lagoslakeid", "sampledate", "secchi")])
+sec.dat = na.omit(data[,c("lagoslakeid", "year", "secchi")])
 
-#set up a vector of different sample sizes that we want to use - # of years sampled from lakes that have at least 20 years of data
-n=c(2, 5, 10, 15, 20)
 
 #calculate actual median and variance for lakes with at least 20 years of data, save vector of lakeids for those lakes to limit sample to them.
 
-ADD BASIC SUMMARIZE HERE
+cv <- function(x) {return(sd(x)/mean(x))}
 
-#set up dummy vectors to store the average median and average variance for each n, these will be as long as n becuase we'll return a single median and a single variance for each n that is the median of the 1000 iterations below
-chla.median.median = numeric(length(n))
-chla.variance.median = numeric(length(n))
+lakeyrmed<- sec.dat %>% group_by(lagoslakeid, year) %>% summarise(ly.med=median(secchi, na.rm=T))
+lakemed<- lakeyrmed %>% group_by(lagoslakeid) %>% summarise(l.med=median(ly.med), l.cv=cv(ly.med), count=n())
+yrs20<- filter(lakemed, count>19)
+lakekeeps<-unique(yrs20$lagoslakeid)
+
+sec20 <- sec.dat[sec.dat$lagoslakeid %in% lakekeeps, ]
+
+
+#set up a vector of different sample sizes that we want to use - # of years sampled from lakes that have at least 20 years of data
+n=c(2, 5, 10, 15, 20, 40, 60, 100)
+
+#data frame to store medians for each lake and sample size combo
+lake.ss<- data.frame(med2obs=numeric(),
+                     med5obs=numeric(),
+                     med10obs=numeric(),
+                     med15obs=numeric(),
+                     med20obs=numeric(),
+                     med40obs=numeric(),
+                     med60obs=numeric(),
+                     med100obs=numeric())
+
+for (k in 1:length(lakekeeps)) {
+  
+  id<-lakekeeps[k]
+  dat.id<-sec.dat[sec.dat$lagoslakeid==id,]
+  med.t<-median(dat.id$secchi)
+  
+  
+  #vector to store data to populate each row of lake.ss
+  lakemeds=numeric(8)
 
 for (j in 1:length(n)) {
   
-  #set up dummy vectors to store median and variance estimates from the inner loop
-  chla.median = numeric(1000)
-  #chla.variance = numeric(1000)
+  #dummy vector to store median from inner loop
+  sample.med=numeric(1000)
   
   for (i in 1:1000) {
     
-    #sample n elements of the chla dataset and take the median, repeat 1,000 times to fill the chla.median and chla.variance vectors
-    sample.x = sample(lagoschla, n[j], replace=F)
-    chla.median[i] = median(sample.x)
-    #chla.variance[i] = sd(sample.x)
+    #sample n observations of 20 year secchi dataset and take the median and cv, repeat 1,000 times to fill the chla.median and chla.variance vectors
+    sample.x = sample(dat.id$secchi, n[j], replace=T)
+    sample.med[i]=median(sample.x)
   }
   
-  #take a median of the 1000 iterations for chla.median and a median of the 1000 iterations for chla.variance to summarize for each n				
-  chla.median.median[j] = median(chla.median)
-  #chla.variance.median[j] = median(chla.variance)
-  chla.variance.median[j] = sd(chla.median)
+  lakemeds[j]= (median(sample.med)-med.t)/med.t*100
+  		
 }
+  
+  lake.ss[k,]=lakemeds
+  
+}
+  
 
-#the actual mean and sd of all of the lagos chla data are 5.9 and 31.8 respectively. check out how the resampled data compared (vectors with 8 values for the 8 values of n)
-chla.median.median
-chla.variance.median
+library(vioplot)
 
-#plot the means and variance by sample size
+vio2<-lake.ss$med2obs
+vio5<-lake.ss$med5obs
+vio10<-lake.ss$med10obs
+vio15<-lake.ss$med15obs
+vio20<-lake.ss$med20obs
+vio40<-lake.ss$med40obs
+vio60<-lake.ss$med60obs
+vio100<-lake.ss$med100obs
 
-par(mfrow=c(2,1))
-barplot(chla.median.median, names=n, main="Median chla by sample size")
-barplot(chla.variance.median, names=n, main="SD chla by sample size")
+vioplot(vio2, vio5, vio10, vio15, vio20, vio40, vio60, vio100, names=c("2", "5", "10", "15", "20", "40", "60", "100"), 
+        col="lightgrey")
+abline(h=0)
 
-#medians are basically the same regardless of how much we resample
-#variance increases from ~25 in low sample to 31 in higher samples.  This makes sense given how skewed the data are, our chances of having low variance in a sample of 25 is higher than 3200 becuase the 25 samples are unlikely to capture the shape of the chla data distribution very well but the 3200 are.
+mat.ss<-as.matrix(lake.ss)
+boxplot.matrix(mat.ss)
+
