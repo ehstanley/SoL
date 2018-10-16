f12ef5ccb322a870a71d4142a5107492d40f0a52
nrlottig <nrlottig@wisc.edu>
2018-10-16 15:22:10 -0500

    updated figures

--- SoL_code/Unbiased_NRL.R
+++ SoL_code/Unbiased_NRL.R
@@ -45,6 +45,8 @@ summer.dat <- c.data %>% filter(day>=166 & day<=258) %>%
   summarise_all(funs(median(., na.rm = TRUE))) %>% 
   left_join(dat_locus)
 
+dat.full <- data.frame(paremeter=NA,cluster=NA,key=NA,value=NA)
+dat.full <- dat.full[-1,]
 #get variable specific dataframe (select which variable in var.dat)
 parameter = names(summer.dat)[c(2:6,8:13)]
 for(i in 1:length(parameter)){
@@ -54,14 +56,16 @@ names(var.dat)[2] <- "value"
 #get sample frequency for observation dataset
 obs <- table(var.dat$group)/sum(table(var.dat$group))
 table(var.dat$group)
-#plot biases
+#data for area plot biases
+param = parameter[i]
 dat.overview = data.frame(cluster=1:16,full=as.vector(full),obs=as.vector(obs))
-dat.overview <- dat.overview %>% gather(key = "key",value = "value",-cluster)
-# p1 <- ggplot(dat.overview, aes(fill=key, y=value, x=cluster)) +
-#   geom_bar(position="dodge", stat="identity") +labs(y="percent", 
-# x="size bin") + 
-#   theme(plot.title = element_text(hjust = 0.5))
-# p1
+dat.overview <- dat.overview %>% 
+  gather(key = "key",value = "value",-cluster) %>% 
+  mutate(variable = as.character(param)) %>% 
+  select(variable, cluster, key, value)
+
+  dat.full <- rbind(dat.full,dat.overview)
+
 
 #stratified sub-sample
 num.samples = 1000  #how many total samples to get
@@ -77,19 +81,6 @@ dat = data.frame(value=c(sample(var.dat$value,size = num.samples,replace = FALSE
                  group=rep(c("Observed","Unbiased"), 
                            c(1000,
                              length (var.unbiased$value))))
-# # p2 <- ggplot(dat, aes(value+.1, fill=group, colour=group)) +
-#   stat_ecdf(geom="step") +
-#   theme_bw() + 
-#   labs(y="ECDF", 
-#                   x=paste(parameter[i],"value",sep=""), 
-#        title="Comparison of Unbiased Distribution") + 
-#   theme(plot.title = element_text(hjust = 0.5)) + scale_x_log10()
-# p2
-
-# # p3 <- ggplot(dat, aes(x=group, y=value+.1, color=group)) + 
-#   geom_violin() + theme_bw() + 
-#   geom_boxplot(width=0.1) + scale_y_log10()
-# p3
 
 assign(x = parameter[i],value = dat)
 summary.stats <- tapply(dat$value, dat$group, summary)
@@ -108,13 +99,11 @@ if(nrow(summary.output)>=2) {
   summary.output = rbind(summary.output,summary.table)
 } else summary.output = summary.table
 
-# plots <- plot_grid(p1,p2,p3,align="h",nrow = 3,ncol = 1)
-# save_plot(paste("SoL_graphics/",parameter[i],"_stratified.png",sep=""),
-          # plots,base_aspect_ratio = 1.3,nrow=3,ncol=1,
-          # base_width = 6)
 }
 
 write_csv(x = summary.output,"SoL_data/unbiased_stats.csv")
+
+#########Unbiased Figure
 
 p1 <- ggplot(data = chla_med,aes(value + 0.1, color=group,)) + stat_ecdf(geom="step") +
   theme_bw() + theme(legend.position = "none") +
@@ -167,6 +156,34 @@ plots <- grid.arrange(p1,p2,p3,p4,p5,p6,ncol=3)
 # plots <- plot_grid(p1,p2,p3,p4,p5,p6, align = "v",nrow = 2)
 plots
 ggsave(plot = plots,filename = "SoL_graphics/unbiased_distributions.tiff",
+       device = "tiff",
+       width = 7.5,
+       height = 5,
+       dpi = 300,
+       units = "in",
+       compression="lzw")
+#Area Bias Figure
+#
+#limit to variables of interest
+dat.full <- dat.full %>% filter(variable!= "tkn_med") %>%  
+  filter(variable!="tn_calculated_med") %>% 
+  filter(variable!= "tn_med")
+dat.full$var2 <- factor(dat.full$variable, labels = c("Chla","Color","DOC","NH4","NO3","Secchi","TN","TP"))
+
+p7 <- ggplot(dat.full, aes(fill=key, y=value, x=cluster)) +
+  geom_bar(position="dodge", stat="identity") + facet_wrap(vars(var2)) +
+  labs(y="Fraction of Lakes",x="Lake size class") +
+  theme_bw() +
+  theme(legend.position = "none",
+        strip.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text=element_text(size=10,  family="sans"),
+        axis.title=element_text(size=10)) +
+  scale_fill_manual(breaks=c("obs","full"), values = c("grey","black"))
+p7
+
+ggsave(plot = p7,filename = "SoL_graphics/area_histograms.tiff",
        device = "tiff",
        width = 7.5,
        height = 5,
