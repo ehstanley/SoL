dfc73ebea9cd0622889b34a2c6602cb1c243e0ce
nrlottig <nrlottig@wisc.edu>
2019-01-03 09:23:12 -0600

    non-log unbiased figure update

--- SoL_code/Unbiased_NRL.R
+++ SoL_code/Unbiased_NRL.R
@@ -2,8 +2,8 @@ library(LAGOSNE)
 library(tidyverse)
 library(lubridate)
 library(splitstackshape)
-library(gridExtra)
 library(cowplot)
+library(gridExtra)
 
 #create variable for outputing summary data
 summary.output = data.frame(temp=NA)
@@ -45,6 +45,8 @@ summer.dat <- c.data %>% filter(day>=166 & day<=258) %>%
   summarise_all(funs(median(., na.rm = TRUE))) %>% 
   left_join(dat_locus)
 
+dat.full <- data.frame(paremeter=NA,cluster=NA,key=NA,value=NA)
+dat.full <- dat.full[-1,]
 #get variable specific dataframe (select which variable in var.dat)
 parameter = names(summer.dat)[c(2:6,8:13)]
 for(i in 1:length(parameter)){
@@ -54,14 +56,22 @@ names(var.dat)[2] <- "value"
 #get sample frequency for observation dataset
 obs <- table(var.dat$group)/sum(table(var.dat$group))
 table(var.dat$group)
-#plot biases
+#data for area plot biases
+param = parameter[i]
 dat.overview = data.frame(cluster=1:16,full=as.vector(full),obs=as.vector(obs))
+dat.overview <- dat.overview %>% 
+  gather(key = "key",value = "value",-cluster) %>% 
+  mutate(variable = as.character(param)) %>% 
+  select(variable, cluster, key, value)
+
+  dat.full <- rbind(dat.full,dat.overview)
+
 dat.overview <- dat.overview %>% gather(key = "key",value = "value",-cluster)
-# p1 <- ggplot(dat.overview, aes(fill=key, y=value, x=cluster)) +
-#   geom_bar(position="dodge", stat="identity") +labs(y="percent", 
-# x="size bin") + 
-#   theme(plot.title = element_text(hjust = 0.5))
-# p1
+p1 <- ggplot(dat.overview, aes(fill=key, y=value, x=cluster)) +
+  geom_bar(position="dodge", stat="identity") +labs(y="percent", 
+x="size bin") + 
+  theme(plot.title = element_text(hjust = 0.5))
+p1
 
 #stratified sub-sample
 num.samples = 1000  #how many total samples to get
@@ -74,24 +84,24 @@ var.unbiased = stratified(indt = var.dat, #random stratification
                           replace = TRUE)
 dat = data.frame(value=c(sample(var.dat$value,size = num.samples,replace = FALSE),
                          var.unbiased$value), 
-                 group=rep(c("Observed","Unbiased"), 
+                 group=rep(c("Observed","Corrected"), 
                            c(1000,
                              length (var.unbiased$value))))
-# # p2 <- ggplot(dat, aes(value+.1, fill=group, colour=group)) +
-#   stat_ecdf(geom="step") +
-#   theme_bw() + 
-#   labs(y="ECDF", 
-#                   x=paste(parameter[i],"value",sep=""), 
-#        title="Comparison of Unbiased Distribution") + 
-#   theme(plot.title = element_text(hjust = 0.5)) + scale_x_log10()
-# p2
 
-# # p3 <- ggplot(dat, aes(x=group, y=value+.1, color=group)) + 
-#   geom_violin() + theme_bw() + 
-#   geom_boxplot(width=0.1) + scale_y_log10()
-# p3
+p2 <- ggplot(dat, aes(value+.1, fill=group, colour=group)) +
+  stat_ecdf(geom="step") +
+  theme_bw() + 
+  labs(y="ECDF", 
+                  x=paste(parameter[i],"value",sep=""), 
+       title="Comparison of Unbiased Distribution") + 
+  theme(plot.title = element_text(hjust = 0.5)) + scale_x_log10()
+p2
 
-assign(x = parameter[i],value = dat)
+p3 <- ggplot(dat, aes(x=group, y=value+.1, color=group)) + 
+  geom_violin() + theme_bw() + 
+  geom_boxplot(width=0.1) + scale_y_log10()
+p3
+
 summary.stats <- tapply(dat$value, dat$group, summary)
 
 summary.table <- data.frame(param = rep(parameter[i],2),
@@ -108,58 +118,57 @@ if(nrow(summary.output)>=2) {
   summary.output = rbind(summary.output,summary.table)
 } else summary.output = summary.table
 
-# plots <- plot_grid(p1,p2,p3,align="h",nrow = 3,ncol = 1)
-# save_plot(paste("SoL_graphics/",parameter[i],"_stratified.png",sep=""),
-          # plots,base_aspect_ratio = 1.3,nrow=3,ncol=1,
-          # base_width = 6)
+assign(x = parameter[i],value = dat)
 }
 
 write_csv(x = summary.output,"SoL_data/unbiased_stats.csv")
 
+#########Unbiased Figure
+
 p1 <- ggplot(data = chla_med,aes(value + 0.1, color=group,)) + stat_ecdf(geom="step") +
   theme_bw() + theme(legend.position = "none") +
-  xlab(bquote(Chla~(mu*g~L^-1))) + ylab("ECDF") + scale_x_log10() +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black")) +
+  xlab(bquote(Chla~(mu*g~L^-1))) + ylab("ECDF") + #scale_x_log10() +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black")) +
   labs(color="Sample Population") +
   theme(text=element_text(size=10,  family="sans"))
 p1
 p2 <- ggplot(data = tn_calculated_med,aes(value + 0.1, color=group,)) + stat_ecdf(geom="step") +
   theme_bw() + theme(legend.position = "none") +
-  xlab(bquote(Total~Nitrogen~(mu*g~L^-1))) + ylab("ECDF") + scale_x_log10() +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black"))  +
+  xlab(bquote(Total~Nitrogen~(mu*g~L^-1))) + ylab("ECDF") + #scale_x_log10() +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black"))  +
   theme(text=element_text(size=10,  family="sans"))
 p2
 p3 <- ggplot(data = doc_med,aes(value + 0.1, color=group,)) + stat_ecdf(geom="step") +
   theme_bw() + theme(legend.position = "none") +
-  xlab(bquote(DOC~(mg~L^-1))) + ylab("ECDF") + scale_x_log10() +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black"))  +
+  xlab(bquote(DOC~(mg~L^-1))) + ylab("ECDF") + #scale_x_log10() +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black"))  +
   theme(text=element_text(size=10,  family="sans"))
 p3
-p4 <- ggplot(chla_med, aes(x=group, y=value+.1, color=group)) +
+p4 <- ggplot(chla_med, aes(x=reorder(group,desc(group)), y=value+.1, color=group)) +
   geom_violin() + theme_bw() +
-  geom_boxplot(width=0.1) + scale_y_log10() +
+  geom_boxplot(width=0.1) + #scale_y_log10() +
   theme_bw() + theme(legend.position = "none") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   ylab(bquote(Chla~(mu*g~L^-1))) + xlab("") +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black")) +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black")) +
   theme(text=element_text(size=10,  family="sans"),axis.title=(element_text(size=10)))
 p4
-p5 <- ggplot(tn_calculated_med, aes(x=group, y=value+.1, color=group)) +
+p5 <- ggplot(tn_combined_med, aes(x=reorder(group,desc(group)), y=value+.1, color=group)) +
   geom_violin() + theme_bw() +
-  geom_boxplot(width=0.1) + scale_y_log10() +
+  geom_boxplot(width=0.1) + #scale_y_log10() +
   theme_bw() + theme(legend.position = "none") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   ylab(bquote(Total~Nitrogen~(mu*g~L^-1))) + xlab("") +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black")) +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black")) +
   theme(text=element_text(size=10,  family="sans"),axis.title=(element_text(size=10)))
 p5
-p6 <- ggplot(doc_med, aes(x=group, y=value+.1, color=group)) +
+p6 <- ggplot(doc_med, aes(x=reorder(group,desc(group)), y=value+.1, color=group)) +
   geom_violin() + theme_bw() +
-  geom_boxplot(width=0.1) + scale_y_log10() +
+  geom_boxplot(width=0.1) + #scale_y_log10() +
   theme_bw() + theme(legend.position = "none") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   ylab(bquote(DOC~(mg~L^-1))) + xlab("") +
-  scale_color_manual(breaks=c("Observed","Unbiased"), values = c("grey","black")) +
+  scale_color_manual(breaks=c("Observed","Corrected"), values = c("grey","black")) +
   theme(text=element_text(size=10,  family="sans"),axis.title=(element_text(size=10)))
 p6
 
@@ -167,6 +176,34 @@ plots <- grid.arrange(p1,p2,p3,p4,p5,p6,ncol=3)
 # plots <- plot_grid(p1,p2,p3,p4,p5,p6, align = "v",nrow = 2)
 plots
 ggsave(plot = plots,filename = "SoL_graphics/unbiased_distributions.tiff",
+       device = "tiff",
+       width = 7.5,
+       height = 5,
+       dpi = 300,
+       units = "in",
+       compression="lzw")
+#Area Bias Figure
+#
+#limit to variables of interest
+dat.full <- dat.full %>% filter(variable!= "tkn_med") %>%  
+  filter(variable!="tn_calculated_med") %>% 
+  filter(variable!= "tn_med")
+dat.full$var2 <- factor(dat.full$variable, labels = c("Chla","Color","DOC","NH4","NO3","Secchi","TN","TP"))
+
+p7 <- ggplot(dat.full, aes(fill=key, y=value, x=cluster)) +
+  geom_bar(position="dodge", stat="identity") + facet_wrap(vars(var2)) +
+  labs(y="Fraction of Lakes",x="Lake size class") +
+  theme_bw() +
+  theme(legend.position = "none",
+        strip.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text=element_text(size=10,  family="sans"),
+        axis.title=element_text(size=10)) +
+  scale_fill_manual(breaks=c("obs","full"), values = c("grey","black"))
+p7
+
+ggsave(plot = p7,filename = "SoL_graphics/area_histograms.tiff",
        device = "tiff",
        width = 7.5,
        height = 5,
