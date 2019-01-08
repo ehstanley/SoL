1a2d6d0daded18a78de9c387198ef81cd80a76ed
limnoliver <skoliver@wisc.edu>
2019-01-08 11:24:43 -0600

    Made changes from reviewers' suggestions including changing census to a star symbol and adding minor ticks on log scales. Also changed spacing between labels.

--- SoL_code/dotplots.R
+++ SoL_code/dotplots.R
@@ -139,7 +139,7 @@ par(mfrow=c(1,3), oma = c(1.5,5.5,2,.5),mar=c(2,5,0,0),cex.axis=1)
 #Trophic Variables
 dotchart2(pop_summ$prop_count, labels = pop_summ$state_name,dotsize = dot_size, xlab = "", 
           xlim = c(-0.02, 0.4), bty= "L", width.factor = .2, col = add.alpha('black', 0.7),
-          cex.labels = 0.7)
+          cex.labels = 0.7, pch = 8)
 dotchart2(secchi_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[1])
 dotchart2(tp_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[2])
 dotchart2(chl_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[3])
@@ -151,7 +151,7 @@ dotchart2(pop_summ$prop_count, labels = "",
           xlab = "", 
           xlim = c(-.02, 0.4), 
           bty= "L", width.factor = .2, 
-          col = add.alpha('black', 0.7))
+          col = add.alpha('black', 0.7), pch = 8)
 dotchart2(tn_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[4])
 dotchart2(no3_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[5])
 dotchart2(nh4_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[6])
@@ -164,20 +164,57 @@ dotchart2(pop_summ$prop_count, labels = "",
           xlab = "", 
           xlim = c(-.02, 0.4), 
           bty= "L", width.factor = .2, 
-          col = add.alpha('black', 0.7))
+          col = add.alpha('black', 0.7), pch = 8)
 dotchart2(doc_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[7])
 dotchart2(color_summ$prop_count,  dotsize = dot_size, add = TRUE, col = my.cols[8])
 box(lwd=1)
 
 par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
 plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-legend(-1.08,1.1, legend = c("Census", "Secchi", "TP","Chl", 
-                            "TN","NO3", "NH4",
+legend(-0.65,1.1, legend = c("Census", "Secchi", "TP","Chl", 
+                            "TN", "NO3", "NH4",
                             "DOC", "Color"), 
-       pch = 16, pt.cex = 1.5, bty = "n", col = c("black", my.cols), cex =1, horiz = TRUE)
+       text.width = c(0, 0.16, 0.16, 0.13, 0.12, 0.11, 0.11, 0.11, 0.11),
+       pch = c(8, rep(16, 8)), pt.cex = 1.5, bty = "n", col = c("black", my.cols), cex =1, horiz = TRUE)
 
 dev.off()}
 # lake area
+
+# minor ticks for log scale function, edited version of 
+# function found here:  https://stackoverflow.com/questions/6955440/displaying-minor-logarithmic-ticks-in-x-axis-in-r
+
+minor.ticks.axis <- function(ax,n,t.ratio=0.5,mn,mx,...){
+  
+  lims <- par("usr")
+  if(ax %in%c(1,3)) lims <- lims[1:2] else lims[3:4]
+  
+  major.ticks <- mn:mx
+  if(missing(mn)) mn <- min(major.ticks)
+  if(missing(mx)) mx <- max(major.ticks)
+  
+  major.ticks <- major.ticks[major.ticks >= mn & major.ticks <= mx]
+  
+  labels <- sapply(major.ticks,function(i)
+    as.expression(bquote(10^ .(i)))
+  )
+  axis(ax,at=major.ticks,labels=FALSE,...)
+  
+  n <- n+2
+  minors <- log10(pretty(10^major.ticks[1:2],n))-major.ticks[1]
+  minors <- minors[-c(1,n)]
+  
+  minor.ticks = c(outer(minors,major.ticks,`+`))
+  minor.ticks <- minor.ticks[minor.ticks > mn & minor.ticks < mx]
+  
+  
+  axis(ax,at=minor.ticks,tcl=par("tcl")*t.ratio,labels=FALSE)
+}
+
+x <- 10^(0:8)
+y <- 1:9
+plot(log10(x),y,xaxt="n",xlab="x",xlim=c(0,9))
+minor.ticks.axis(1,9,mn=0,mx=8)
+
 tiff(filename = "SoL_graphics/dotplots_area.tiff",
      width = 3.5,
      height = 3.5,
@@ -188,18 +225,21 @@ tiff(filename = "SoL_graphics/dotplots_area.tiff",
 par(mfrow=c(1,1), oma = c(1.5,5.5,2,.5),mar=c(2,5,0,0),cex.axis=1)
 dotchart2(log10(pop_summ$lake_area_ha), labels = pop_summ$state_name,dotsize = 1.5, xlab = "", 
           xlim = c(0.9, 4.1), bty= "L", width.factor = .2, col = add.alpha('black', 0.7),
-          cex.labels = 0.7,axisat = c(0:4), axislabels = c('1', '10', '100', '1000', '10000'))
+          cex.labels = 0.7,axisat = c(0:4), axislabels = c('1', '10', '100', '1000', '10000'),
+          pch = 8)
+minor.ticks.axis(1,9,mn=1,mx=4)
+
 dotchart2(log10(chl_summ$lake_area_ha),  dotsize = 1.5, add = TRUE, col = my.cols[2])
 dotchart2(log10(tn_summ$lake_area_ha),  dotsize = 1.5, add = TRUE, col = my.cols[4])
 dotchart2(log10(doc_summ$lake_area_ha),  dotsize = 1.5, add = TRUE, col = my.cols[7])
 box(lwd=1)
 mtext(side=1,text = "Area (ha)",line=2.2)
+
 par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
 plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-legend(-1.,1.1, legend = c("Census", "Chl", 
-                             "TN",
-                             "DOC"), 
-       pch = 16, pt.cex = 1.3, bty = "n", col = c("black", my.cols[c(2,4,7)]), cex =1, horiz = TRUE)
+legend(-0.36,1.1, legend = c("Census", "Chl", "TN", "DOC"), 
+       text.width = c(0, 0.28, 0.2, 0.17),
+       pch = c(8,rep(16,3)), pt.cex = 1.3, bty = "n", col = c("black", my.cols[c(2,4,7)]), cex =1, horiz = TRUE)
 dev.off()
 
 
